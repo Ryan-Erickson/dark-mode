2321516ab3f3870a9d03e59a553e4ce86a5991cd
Ryan Erickson <rerickson21@yahoo.com>
2020-04-15 17:33:36 -0400

    MVP

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import {useDarkMode} from '../hooks/useDarkMode';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
@@ -18,5 +19,4 @@ const Navbar = () => {
     </nav>
   );
 };
-
 export default Navbar;
